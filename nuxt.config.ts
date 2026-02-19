cd4e973e07a8164a6b1ee7bbd29383c0cd25edd8
magixe <emax.mails@gmail.com>
2026-02-19 06:06:21 +0500

    Static build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,24 @@ export default defineNuxtConfig({
 	icon: {
 		serverBundle: 'local'
 	},
+	app: {
+		head: {
+			htmlAttrs: {
+				lang: 'ru'
+			},
+			link: [
+				{
+					rel: 'icon',
+					type: 'image/x-icon',
+					href: '/favicon.ico'
+				},
+				{
+					rel: 'canonical',
+					href: 'https://tatyana-arkhipova.ru/'
+				}
+			]
+		}
+	},
 	devtools: { enabled: false },
 	ui: {
 		colorMode: false
